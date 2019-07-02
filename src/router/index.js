9cee1847dc5fdde49be1308e3683a496b19fab2f
cbydpi <960576010@qq.com>
2019-07-02 13:51:10 +0800

    海淀园区布置完成

--- src/router/index.js
+++ src/router/index.js
@@ -74,7 +74,7 @@ router.beforeEach((to, from, next) => {
 //        }
 //      }
 
-        window.location.href = 'https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx57c02bfc15411a90&redirect_uri=http%3A%2F%2Fresource.ofaai.com%2Ftest%2F%23%2F'+to.path.substr(1)+'&response_type=code&scope=snsapi_userinfo&state='+state+'#wechat_redirect'
+        window.location.href = 'https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx57c02bfc15411a90&redirect_uri=http%3A%2F%2Fwx.ofaai.com%2Fpolicy%2F%23%2F'+to.path.substr(1)+'&response_type=code&scope=snsapi_userinfo&state='+state+'#wechat_redirect'
       } else {
         if (router.app.$cookie.get('openid') !== null) {
           next()
